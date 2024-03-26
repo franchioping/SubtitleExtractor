17f7a2b44700dc690a2550a1de651b2886b53c8a
franchioping <franchioping@gmail.com>
2024-03-26 23:48:27 +0000

    cutom fese library + subtitle extraction

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
-# This is a sample Python script.
+import fese
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+videopath = "./files/vid.mkv"
+
+video = fese.FFprobeVideoContainer(videopath)
+
+subtitles = video.get_subtitles()
+
+print(subtitles[0].suffix)
+
+extract = []
+for subtitle in subtitles:
+    if subtitle.language == "en":
+        extract.append(subtitle)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print(video.copy_subtitles(extract))
