6b8a7dd4db6354276631fc410ceb58f78eaeca52
franchioping <franchioping@gmail.com>
2024-03-27 20:10:26 +0000

    use last updated times instead of hashes

--- main.py
+++ main.py
@@ -8,30 +8,15 @@ import xxhash
 import subs
 import pickle
 import json
+import os
 
 from config import *
 
 
-def get_hash(file):
-    CHUNK_SIZE = 2 ** 32  # Or whatever you have memory to handle
-    with open(file, 'rb') as input_file:
-        x = xxhash.xxh3_64()
-        for chunk in iter(partial(input_file.read, CHUNK_SIZE), b''):
-            x.update(chunk)
-        return x.hexdigest()
-
-def sha256sum(filename):
-    with open(filename, 'rb', buffering=0) as f:
-        return hashlib.file_digest(f, 'sha256').hexdigest()
-
-
-VideoSubtitlesMapType = TypedDict('VideoSubtitlesMapType', {'video': Path, 'subs': list[Path]})
-
-
 class FileWatcher:
     def __init__(self):
-        self.processed_video_hashes = []
-        self.video_subtitles_map: VideoSubtitlesMapType = {}
+        self.processed_video_last_updated_times = {}
+        self.video_subtitles_map = {}
 
     def check_files(self):
         files: list[Path] = filter(
@@ -39,33 +24,40 @@ class FileWatcher:
             MEDIA_PATH.rglob('*.mkv')
         )
 
-        hashes_seen = []
+        changed = []
+        seen = []
         for file in files:
-            file_hash = get_hash(file)
-            hashes_seen.append(file_hash)
 
-            if file_hash in self.processed_video_hashes:
-                continue
+            last_updated = os.path.getctime(str(file.absolute()))
+
+            if file in self.processed_video_last_updated_times.keys():
+                if self.processed_video_last_updated_times[file] == last_updated:
+                    continue
+                else:
+                    changed.append(file)
 
             print(f" [*] Processing {file}")
             sub_files = subs.extract_subs(str(file.absolute()))
-            self.video_subtitles_map[file_hash] = [Path(x) for x in list(sub_files.values())]  # type: ignore
-            self.processed_video_hashes.append(file_hash)
+
+            self.video_subtitles_map[file] = [Path(x) for x in list(sub_files.values())]  # type: ignore
+            self.processed_video_last_updated_times[file] = last_updated
+            seen.append(file)
+
             self.save()
             print(f"     - Finished Extracting Subtitles")
 
-        for file_hash in self.video_subtitles_map.keys():
-            if file_hash not in hashes_seen:
+        for file in self.processed_video_last_updated_times.keys():
+            if file not in seen:
 
-                for sub_file in self.video_subtitles_map[file_hash]:  # type: ignore
+                for sub_file in self.video_subtitles_map[file]:
 
                     if [x for xs in list(self.video_subtitles_map.values()) for x in xs].count(sub_file) == 1:
                         sub_file.unlink()
 
-                self.video_subtitles_map[file_hash] = []  # type: ignore
+                self.video_subtitles_map[file] = []
 
-                if file_hash in self.processed_video_hashes:
-                    self.processed_video_hashes.remove(file_hash)
+                if file in self.processed_video_last_updated_times:
+                    self.processed_video_last_updated_times.remove(file)
 
     def save(self):
         with open(SAVEFILE, "wb") as f:
