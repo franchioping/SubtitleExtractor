2759f9b0d59d64713f888cc650f8a38ad5ff9f55
franchioping <franchioping@gmail.com>
2024-03-28 20:46:24 +0000

    Use logging instead of print

--- main.py
+++ main.py
@@ -9,8 +9,19 @@ import subs
 import pickle
 import json
 import os
+import logging
 
 from config import *
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(message)s",
+    handlers=[
+        logging.FileHandler("SubtitleExtractor.log"),
+        logging.StreamHandler()
+    ]
+)
+
 
 
 class FileWatcher:
@@ -36,7 +47,7 @@ class FileWatcher:
                 else:
                     changed.append(file)
 
-            print(f" [*] Processing {file}")
+            logging.info(f" [*] Processing {file}")
             sub_files = subs.extract_subs(str(file.absolute()))
 
             self.video_subtitles_map[file] = [Path(x) for x in list(sub_files.values())]  # type: ignore
@@ -44,15 +55,17 @@ class FileWatcher:
             seen.append(file)
 
             self.save()
-            print(f"     - Extracted Files: {sub_files.values()}")
-            print(f"     - Finished Extracting Subtitles")
+            logging.info(f"     - Extracted Subs ({len(list(sub_files.values()))}): {sub_files.values()}")
 
+        logging.info("Clearing unused subs")
         for file in self.processed_video_last_updated_times.keys():
             if file not in seen:
+                logging.info(f" [*] File {file} was removed, deleting subtitles")
 
                 for sub_file in self.video_subtitles_map[file]:
 
                     if [x for xs in list(self.video_subtitles_map.values()) for x in xs].count(sub_file) == 1:
+                        logging.info(f"      - Deleted {sub_file}")
                         sub_file.unlink()
 
                 self.video_subtitles_map[file] = []
@@ -74,7 +87,7 @@ class FileWatcher:
 
 def main():
     if Path(LOCKFILE).exists():
-        print("program is already running")
+        logging.warning("program is already running")
         return -1
     open(LOCKFILE, "w").close()
     watcher = FileWatcher.load()
@@ -85,8 +98,11 @@ def main():
 
 if __name__ == "__main__":
     try:
+        logging.info("Starting Program")
         main()
-    except Exception:
+    except Exception as e:
+        logging.error(f"Exception {e}, Terminating")
         Path(LOCKFILE).unlink()
     except KeyboardInterrupt:
+        logging.error("Keyboard Interrupt, Terminating")
         Path(LOCKFILE).unlink()
